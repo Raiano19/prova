ca4816eadd5f4811f729a9814b1699a60191360e
Raiano19 <121039987+Raiano19@users.noreply.github.com>
2024-01-05 15:23:31 -0300

    Add files via upload

---
+++ prova/questao01/index.js
@@ -0,0 +1,11 @@
+const endereço=[
+    rua= 'Rua das Flores', cidade= 'Porto Alegre', CEP='90000-000'
+
+
+]
+
+const [detalhar1,detralahr2,detalhar3] = endereço
+const escrito =
+
+
+console.log("rua:"+detalhar1+" cidade:"+detralahr2+" cep:"+detalhar3)
---
+++ prova/questao02/index.js
@@ -0,0 +1,15 @@
+const nome= [
+    'Carlos Alberto Silva',
+    'Julia Pereira Gomes', 'Lucas Alves'
+]
+
+const abreviarNomes = function(nome){
+ nome.map(nome.length -1 ) 
+    return;
+ 
+
+}
+
+
+
+ console.log(nome)
